50f0700f1429e59f23ad67c42abf2c9193bded99
mishina <mishina@localhost.com>
2020-11-16 05:27:05 +0900

    commit

--- detect.py
+++ detect.py
@@ -236,7 +236,7 @@ def detect(save_img=False):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='mask9.pt', help='model.pt path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default='mask350.pt', help='model.pt path(s)')
     parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
     parser.add_argument('--output', type=str, default='inference/output', help='output folder')  # output folder
     parser.add_argument('--img-size', type=int, default=480, help='inference size (pixels)')
